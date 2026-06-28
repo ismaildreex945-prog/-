191c2dfdafa498c0b2b2d03c35dc5c11e670bbc0
ismaildreex945-prog <ismaildreex945@gmail.com>
2026-06-28 15:04:06 +0200

    Update booot.py

--- booot.py
+++ booot.py
@@ -1,11 +1,4 @@
 import mysql.connector
-mydb = mysql.connector.connect(
-  host="mysql-10c62286-ismaildreex945-76ee.h.aivencloud.com",
-  port=23045,
-  user="avnadmin",
-  password="AVNS_87pPrZKA-h8TKh3Jg7k",
-  database="defaultdb"
-)
 from groq import Groq
 
 from telegram import Update, ReplyKeyboardMarkup
@@ -27,7 +20,13 @@ client = Groq(
     api_key="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"
 )
 
-db = mydb
+db = mysql.connector.connect(
+  host="mysql-10c62286-ismaildreex945-76ee.h.aivencloud.com",
+  port=23045,
+  user="avnadmin",
+  password="AVNS_87pPrZKA-h8TKh3Jg7k",
+  database="defaultdb"
+)
 
 cursor = db.cursor()
 
